2b6d0006f37c87ddfe70edb81d6fdd3a2973f8b9
Kathleen <kathleenbucarito@gmail.com>
2021-08-02 13:09:38 -0300

    Segundo commit mejorando version movil

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $(function(){
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+
+    if(window.innerWidth<380){
+        $('h1').addClass('pt-5')
+        $('.btn').addClass('btn-sm m-1');
+        $('.btn').removeClass('btn-block');
+    }
 })
