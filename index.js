ead14f89c97110a5d08d775dc605f38c91f31b57
Kathleen <kathleenbucarito@gmail.com>
2021-08-02 14:01:09 -0300

    'Cuarto'

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
 
-    if(window.innerWidth<301){
-        $('h1').addClass('h5')
-        $('.btn').addClass('btn-sm m-1');
-        $('.btn').removeClass('btn-block');
+    if (window.innerWidth < 415) {
+        $('h1').addClass('h4 mt-5 pt-3');
+        $('.btn').addClass('my-1')
     }
 })
