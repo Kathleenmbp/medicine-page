b7a67d4974f5e25c04b89e122b24dd4df297473b
Kathleen <kathleenbucarito@gmail.com>
2021-08-02 13:43:33 -0300

    'Tercero'

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
 
-    if(window.innerWidth<380){
-        $('h1').addClass('pt-5')
+    if(window.innerWidth<301){
+        $('h1').addClass('h5')
         $('.btn').addClass('btn-sm m-1');
         $('.btn').removeClass('btn-block');
     }
